1289ec0fe11cbd7467c437c05f3a01d863eca0b5
nomanjanovjahongir-beep <nomanjanovjahongir@gmail.com>
2026-07-02 12:44:37 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# ============ BOT SOZLAMALARI ============
+# ✏️ O'ZGARTIRISH KERAK! - O'z tokeningizni yozing
+BOT_TOKEN = "8901157648:AAER_bR0ntOjcaxDflrtCfFyRXd55qxl5to"
+
+# ✏️ O'ZGARTIRISH KERAK! - Bot username (@ belgisiz)
+BOT_USERNAME = "toxicbullpromo_bot"
+
+# ============ MAJBURIY KANALLAR ============
+# ✏️ O'ZGARTIRISH KERAK! - O'z kanallaringizni yozing
+REQUIRED_CHANNELS = {
+    "Kanal 1": "@englishwithjkk",
+}
+
+# ============ DATABASE ============
+DATABASE_FILE = "users.db"
+
+# ============ REFERRAL SOZLAMALARI ============
+REFERRAL_REWARD = 1
+
+# ============ PROMO NARXLAR ============
+PROMO_PRICES = {
+    "69 promo": 10,
+    "99 promo": 15,
+    "299 promo": 25,
+}
+
+# ============ ADMIN ============
+ADMIN_ID = 8632521282
